8ced178d50d74d040885b2b4a9df84a678241d18
MrPaul007 <75275133+MrPaul007@users.noreply.github.com>
2022-08-21 12:30:21 +0300

    ready

---
+++ index.js
@@ -0,0 +1,30 @@
+const contacts = require("./contacts");
+const { listContacts, getContactById, removeContact, addContact } = contacts;
+
+const argv = require("yargs").argv;
+
+// TODO: рефакторить
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      listContacts().then(data => console.table(data));
+      break;
+
+    case "get":
+      getContactById(id).then(data => console.table(data));
+      break;
+
+    case "add":
+      addContact(name, email, phone);
+      break;
+
+    case "remove":
+      removeContact(id);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
